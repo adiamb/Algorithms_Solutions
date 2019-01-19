2d6431772464eba46a7370ea0fdbc04b5051de4a
Aditya Ambati <ambati@stanford.edu>
2019-01-19 14:40:26 -0800

    Create DecompressString.py

---
+++ DecompressString.py
@@ -0,0 +1,18 @@
+def decompress(Str, Ind):
+"""Take a nested string in the format '2[ab2[c]d]' and expand it into 'abccdabccd'"""
+	i = Ind
+	Sub = ''
+	CurStr = ''
+	while i < len(Str):
+		if Str[i].isdigit():
+			Sub += Str[i]
+		elif Str[i].isalpha(): 
+			CurStr += Str[i]
+		elif Str[i] == ']':
+			return CurStr, i
+		else:## start recursion here
+			NCurStr, i = decompress(Str, i+1)
+			CurStr += NCurStr*int(Sub)
+			Sub = ''
+		i += 1
+	return CurStr
